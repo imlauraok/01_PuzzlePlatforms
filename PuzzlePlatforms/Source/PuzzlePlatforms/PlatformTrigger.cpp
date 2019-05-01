83e32f335a921ce4b47cf9ac372df7c83f57dc8d
Mimikyu <imlauraok@gmail.com>
2019-05-01 11:20:35 +0100

    Handling Overlap Events in C++

--- PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -20,6 +20,8 @@ void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
@@ -29,3 +31,12 @@ void APlatformTrigger::Tick(float DeltaTime)
 
 }
 
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
+}
